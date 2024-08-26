097547db6329148edd8ce7c174a6c0e65b5628ed
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2024-08-26 23:29:52 +0100

    done with landing page layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //import logo from './logo.svg';
 import LoginSignup from "./pages/LoginSignup";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import About from "./components/About";
 import Work from "./components/Work";
 import Testimonial from "./components/Testimonial";
-import Contact from "./pages/Contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
